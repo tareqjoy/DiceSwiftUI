e9e6fcff018509afab7d01985cca00e76b5010ef
tareqjoy <me.tareqjoy@gmail.com>
2020-04-29 21:30:45 -0700

    Initial Commit

---
+++ DiceSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceSwiftUI
+//
+//  Created by Tareq Rahman Joy on 29/4/20.
+//  Copyright © 2020 Tareq Rahman Joy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
